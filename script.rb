5301da4b6858bb3fdd318c494cd33dfe6b796211
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2023-03-14 13:33:42 +0700

    Calculate and add profit to each price pair array

--- script.rb
+++ script.rb
@@ -1,11 +1,16 @@
 stock_prices = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(stock_prices)
-   pairs = []
-   stock_prices.combination(2) { |combination| pairs.push(combination) }
-   pairs.keep_if {|buy, sell| sell > buy }
-   p pairs
-   
+  pairs = []
+  stock_prices.combination(2) { |combination| pairs.push(combination) }
+  pairs.keep_if {|buy, sell| sell > buy }
+  p pairs
+
+  pairs.each do |pair| 
+  profit = pair[1] - pair[0]
+  p pair.push(profit)
+  pairs
+  end
 end
 
 p stock_picker(stock_prices)

45b238008a3cbe7da3c6edc04bd88dd36f9d45c7
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2023-03-14 13:25:36 +0700

    Add method to create array of arrays with all number pair combinations from original stock prices array

---
+++ script.rb
@@ -0,0 +1,10 @@
+stock_prices = [17,3,6,9,15,8,6,1,10]
+
+def stock_picker(stock_prices)
+   pairs = []
+   stock_prices.combination(2) { |combination| pairs.push(combination) }
+   pairs.keep_if {|buy, sell| sell > buy }
+   p pairs
+end
+
+p stock_picker(stock_prices)

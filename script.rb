873a72fd23cd559daff101c3003899dd6617cb82
JLopez-W <102275514+JLopez-W@users.noreply.github.com>
2023-03-14 13:31:16 +0700

    Add method to compare all price pair combinations
    Add array of array of every price combination from stock_prices array
    
    Check the price difference of each pair and keep only the ones that have a profit

--- script.rb
+++ script.rb
@@ -5,6 +5,7 @@ def stock_picker(stock_prices)
    stock_prices.combination(2) { |combination| pairs.push(combination) }
    pairs.keep_if {|buy, sell| sell > buy }
    p pairs
+   
 end
 
 p stock_picker(stock_prices)
